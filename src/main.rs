ed273aaf97412a025a39cc668f9e5ee591fbfa9c
tolvanea <alpi.tolvanen@tutanota.com>
2020-11-06 11:42:49 +0200

    Update example code to safe use pin wrappers.

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,27 @@
 #[macro_use]
 extern crate teensy3;
 
-use teensy3::bindings;
 use teensy3::serial::Serial;
-use teensy3::util::{pin_mode, digital_write, PinMode};
+use teensy3::util::{delay};
+use teensy3::pins::{PinMode, Pin, PinRowSingleton};
+use teensy3::bindings;
+
+fn setup() -> PinRowSingleton {
+    // It's unsafe because caller verifies that it's called only once
+    unsafe{PinRowSingleton::new_once()}
+}
+
 
 #[no_mangle]
-pub unsafe extern fn main() {
-    // Blink Loop
-    pin_mode(13, PinMode::Output); // Set led pin to be output
-    digital_write(13, false); // Set led off
+pub extern fn main() {
+    let mut pinrow = setup();
+    let mut led = pinrow.get_led();
+    led.digital_write(false); // Set led off
     let ser = Serial{};
     let mut i = 0;
 
+    // Blink Loop
     loop {
-
         // Send a message over the USB Serial port
         // Print with println! wrapper macro, which just uses serial write on background
         println!("Hello! Count: {}", i);
@@ -25,28 +32,27 @@ pub unsafe extern fn main() {
         ser.write_bytes("Hello !\n".as_bytes()).unwrap_or_else(|_| {println!("Fail!"); ()});
         i += 1;
         // Show we are alive by blinking
-        alive();
+        alive(&mut led);
 
         // Keep 2 second pause in blinking the led, also don't spam the console
-        bindings::delay(2000);
+        delay(2000);
     }
 }
 
 /// Blink the light twice to know we're alive
-pub unsafe fn alive() {
+pub fn alive(led: &mut Pin) {
     // Blink led with custom wrapper
-    for _ in 0..5 {
-        digital_write(13, true);
-        bindings::delay(50);
-        digital_write(13, false);
-        bindings::delay(50);
+    for i in 0..10 {
+        led.digital_write(i%2 == 0);
+        delay(50);
     }
     // Blink led with raw bindings
+    // (Safe wrapper is more recommended because it keep book what pins are in what state)
     for _ in 0..5 {
-        bindings::digitalWrite(13, bindings::HIGH as u8);
-        bindings::delay(50);
-        bindings::digitalWrite(13, bindings::LOW as u8);
-        bindings::delay(50);
+        unsafe{bindings::digitalWrite(13, bindings::HIGH as u8)};
+        unsafe{bindings::delay(50)};
+        unsafe{bindings::digitalWrite(13, bindings::LOW as u8)};
+        unsafe{bindings::delay(50)};
     }
 }
 
