a5b34b11da7417ccdd220928a4f12024e4a9a359
tolvanea <alpi.tolvanen@tutanota.com>
2020-12-01 14:19:37 +0200

    Tune clippy lints

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![no_std]
 #![no_main]
 
+#![deny(unused_must_use)]
+#![allow(clippy::needless_return)]
+
 #[macro_use]
 extern crate teensy3;
 
 mod record_keyboard_matrix;
 mod process_keys;
+mod key_codes;
 
 
 use heapless::{Vec, ArrayLength};  // fixed capacity `std::Vec`
@@ -34,27 +38,30 @@ fn setup() -> PinRow {
 #[no_mangle]
 pub extern fn main() {
     let mut pinrow = setup();
+    let pinrow = &mut pinrow;
     let mut led = pinrow.get_led();
     for _ in 0..2 {
         alive(&mut led);
     }
     println!("Hellouu!");
-
-    let key_codes: &[&[u32]] = &[
-        &[b::KEY_ENTER, b::KEY_SPACE],
-        &[b::KEY_Q, b::KEY_W, b::KEY_R],
-        &[b::KEY_A, b::KEY_S, b::KEY_D],
-    ];
-
-    let key_names: &[&[&str]] = &[
-        &["b::KEY_ENTER", "b::KEY_SPACE"],
-        &["b::KEY_Q", "b::KEY_W", "b::KEY_R"],
-        &["b::KEY_A", "b::KEY_S", "b::KEY_D"],
-    ];
-
-    let mut keymat: KeyMatrix = figure_out_key_matrix(
-        &mut pinrow, key_codes, key_names
+    //use key_codes::{KEY_CODES_SHORT_TEST as KEY_CODES, KEY_NAMES_SHORT_TEST as KEY_NAMES};
+    use key_codes::{KEY_CODES, KEY_NAMES};
+    let mut mat: KeyMatrix = figure_out_key_matrix(
+        pinrow, KEY_CODES, KEY_NAMES
     );
+    //
+    // let code_matrix = [
+    //     [b::KEY_W, b::KEY_R, b::KEY_Q,            0,            0, ],
+    //     [       0,        0,        0, b::KEY_ENTER, b::KEY_SPACE, ],
+    //     [b::KEY_S, b::KEY_D, b::KEY_A,            0,            0, ],
+    // ].iter().map(|v| v.iter().map(|&k| if k==0 { None } else { Some(k) }).collect())
+    //     .collect();
+    // let rows = Vec::from_slice(&[2, 12, 21]).unwrap();
+    // let cols = Vec::from_slice(&[17, 18, 20, 22, 40]).unwrap();
+    // let mut mat = KeyMatrix::new(pinrow, code_matrix, rows, cols);
+
+
+
     // let matrix: &[&[usize]] = [
     //     [       0,        0,        0, b::KEY_ENTER,        0,            0, ],
     //     [       0,        0,        0,            0,        0, b::KEY_SPACE, ],
@@ -68,7 +75,7 @@ pub extern fn main() {
     let mut keyboard = unsafe{b::Keyboard};
     for _ in 0..10000 {
         delay(30);
-        let v = match keymat.scan_key_press() {
+        let v = match mat.scan_key_press() {
             Some(v) => v,
             None => {continue;} // Nothing is pressed
         };
