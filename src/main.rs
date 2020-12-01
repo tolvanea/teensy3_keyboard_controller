767f48eee8c207ec3fd285b4b0f4554ebe744a01
tolvanea <alpi.tolvanen@tutanota.com>
2020-12-01 15:22:57 +0200

    Apply previous rearrangements

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@
 #[macro_use]
 extern crate teensy3;
 
-mod record_keyboard_matrix;
 mod process_keys;
 mod key_codes;
+mod record_keyboard_matrix;
 
 
 use heapless::{Vec, ArrayLength};  // fixed capacity `std::Vec`
@@ -18,8 +18,7 @@ use teensy3::util::{delay};
 use teensy3::pins::{Pin, PinRow};
 use teensy3::bindings as b;
 
-use record_keyboard_matrix::figure_out_key_matrix;
-use process_keys::{KeyMatrix, ScanRowState};
+use process_keys::ScanRowState;
 
 /// Initialise vector filled with some value
 fn full_vec<T, U>(value: T, len: usize) -> Vec<T,U>
@@ -38,39 +37,13 @@ fn setup() -> PinRow {
 #[no_mangle]
 pub extern fn main() {
     let mut pinrow = setup();
-    let pinrow = &mut pinrow;
     let mut led = pinrow.get_led();
     for _ in 0..2 {
         alive(&mut led);
     }
     println!("Hellouu!");
-    //use key_codes::{KEY_CODES_SHORT_TEST as KEY_CODES, KEY_NAMES_SHORT_TEST as KEY_NAMES};
-    use key_codes::{KEY_CODES, KEY_NAMES};
-    let mut mat: KeyMatrix = figure_out_key_matrix(
-        pinrow, KEY_CODES, KEY_NAMES
-    );
-    //
-    // let code_matrix = [
-    //     [b::KEY_W, b::KEY_R, b::KEY_Q,            0,            0, ],
-    //     [       0,        0,        0, b::KEY_ENTER, b::KEY_SPACE, ],
-    //     [b::KEY_S, b::KEY_D, b::KEY_A,            0,            0, ],
-    // ].iter().map(|v| v.iter().map(|&k| if k==0 { None } else { Some(k) }).collect())
-    //     .collect();
-    // let rows = Vec::from_slice(&[2, 12, 21]).unwrap();
-    // let cols = Vec::from_slice(&[17, 18, 20, 22, 40]).unwrap();
-    // let mut mat = KeyMatrix::new(pinrow, code_matrix, rows, cols);
-
-
-
-    // let matrix: &[&[usize]] = [
-    //     [       0,        0,        0, b::KEY_ENTER,        0,            0, ],
-    //     [       0,        0,        0,            0,        0, b::KEY_SPACE, ],
-    //     [       0,        0,        0,            0, b::KEY_Q,            0, ],
-    //     [       0,        0,        0,            0, b::KEY_W,            0, ],
-    //     [       0,        0,        0,            0, b::KEY_R,            0, ],
-    //     [b::KEY_A, b::KEY_S, b::KEY_D,            0,        0,            0, ],
-    // ];
-
+    let mut mat = key_codes::ask_key_codes_and_print_them(&mut pinrow);
+    //let mat = key_codes::get_stored_key_codes(&mut pinrow);
 
     let mut keyboard = unsafe{b::Keyboard};
     for _ in 0..10000 {
